eb887367c29999deb9677a8aafbdf54e2e429293
Dima Lunich <dima.lunich@gmail.com>
2021-01-14 21:14:06 +0100

    Puma added

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'sinatra'
-require 'sinatra/json'
-require 'faker'
+Bundler.require
 
 def user(id:)
   {
@@ -19,14 +17,14 @@ def user(id:)
   }
 end
 
-def users(count:)
+def generate_users(count:)
   count.times.map do |i|
     user(id: i + 1)
   end
 end
 
 def users_response(count: 15, page: 1, per_page: 5)
-  users = users(count: 15)
+  users = generate_users(count: 15)
   offset = per_page * (page - 1)
   {
     result: users[offset, per_page],
@@ -35,14 +33,17 @@ def users_response(count: 15, page: 1, per_page: 5)
   }
 end
 
+before do
+  # Setup random config
+  Faker::Config.random = Random.new(42)
+  # Make it possible to be used from any hostname
+  headers['Access-Control-Allow-Origin'] = '*'
+end
+
 get '/' do
   json name: 'Faker API', version: '0.0.1', description: 'API for my cat to learn'
 end
 
 get '/api/users' do
-  # Setup random config
-  Faker::Config.random = Random.new(42)
-  headers['Access-Control-Allow-Origin'] = '*'
-
   json users_response(page: (params[:page] || '1').to_i)
 end
